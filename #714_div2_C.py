b420b127918aa96fecff23191b8fd15a4c69ab6e
Sangwoo Park <tkddn3367@gmail.com>
2021-04-14 00:55:11 +0900

    21.04.13

--- #714_div2_C.py
+++ #714_div2_C.py
@@ -1,15 +1,18 @@
 import sys
 input = sys.stdin.readline
 MOD = 1000000007
-dp = [1 for _ in range(200010)]
-for i in range(10, 200010):
+dp = [1 for _ in range(200011)]
+for i in range(10, 200011):  # 0을 i번 연산한 값의 자릿수 구하기
     dp[i] = (dp[i-10]+dp[i-9]) % MOD
+
+for i in range(25):
+    print(dp[i], end=" ")
 
 for _ in range(int(input())):
     n, m = map(int, input().split())
     ans = 0
     while n:
-        ans += dp[m+n % 10]
+        ans += dp[m + n % 10]
         n //= 10
         ans %= MOD
     print(ans)
